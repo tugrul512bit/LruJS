b81ef6a358a57c655bd374cd7b0b72b3b0144460
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-04-07 21:36:57 +0300

    Update sample_async_cache_miss_benchmark.js

--- sample_async_cache_miss_benchmark.js
+++ sample_async_cache_miss_benchmark.js
@@ -1,28 +1,27 @@
 "use strict";
-/* benchmark 1000 milisecond item loading asynchronously for 1000 times, in 1100 miliseconds */
-let Lru = require("./lrucache.js").Lru;
+	let Lru = require("./lrucache.js").Lru;
 
-let cache = new Lru(500, async function(key,callback){
-	// cache-miss data-load algorithm
-	setTimeout(function(){
-		callback(key+" processed");
-	},1000);
-},1000 /* cache element lifetime */);
+	let cache = new Lru(1000, async function(key,callback){
+		// cache-miss data-load algorithm
+		setTimeout(function(){
+			callback(key+" processed");
+		},1000);
+	},1000 /* cache element lifetime */);
 
-let ctr = 0;
-let t1 = Date.now();
-for(let i=0;i<1000;i++)
-{
-	cache.get(i,function(data){ 
-		console.log("data:"+data+" key:"+i);
-		if(i.toString()+" processed" !== data)
-		{
-			console.log("error: wrong key-data mapping.");
-			
-		}
-		if(++ctr === 1000)
-		{
-			console.log("benchmark: "+(Date.now()-t1)+" miliseconds");
-		}
-	});
-}
+	let ctr = 0;
+	let t1 = Date.now();
+	for(let i=0;i<1000;i++)
+	{
+		cache.get(i,function(data){ 
+			console.log("data:"+data+" key:"+i);
+			if(i.toString()+" processed" !== data)
+			{
+				console.log("error: wrong key-data mapping.");
+				
+			}
+			if(++ctr === 1000)
+			{
+				console.log("benchmark: "+(Date.now()-t1)+" miliseconds");
+			}
+		});
+	}
