ed7dddb87d65e5278c62768dc0c08f3e61a6ca51
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-04-07 21:37:56 +0300

    Update sample_async_cache_miss_benchmark.js

--- sample_async_cache_miss_benchmark.js
+++ sample_async_cache_miss_benchmark.js
@@ -1,27 +1,28 @@
 "use strict";
-	let Lru = require("./lrucache.js").Lru;
+// number of asynchronous accessors(1000 here) need to be equal to or less than cache size(1000 here) or it makes dead-lock
+let Lru = require("./lrucache.js").Lru;
 
-	let cache = new Lru(1000, async function(key,callback){
-		// cache-miss data-load algorithm
-		setTimeout(function(){
-			callback(key+" processed");
-		},1000);
-	},1000 /* cache element lifetime */);
+let cache = new Lru(1000, async function(key,callback){
+	// cache-miss data-load algorithm
+	setTimeout(function(){
+		callback(key+" processed");
+	},1000);
+},1000 /* cache element lifetime */);
 
-	let ctr = 0;
-	let t1 = Date.now();
-	for(let i=0;i<1000;i++)
-	{
-		cache.get(i,function(data){ 
-			console.log("data:"+data+" key:"+i);
-			if(i.toString()+" processed" !== data)
-			{
-				console.log("error: wrong key-data mapping.");
-				
-			}
-			if(++ctr === 1000)
-			{
-				console.log("benchmark: "+(Date.now()-t1)+" miliseconds");
-			}
-		});
-	}
+let ctr = 0;
+let t1 = Date.now();
+for(let i=0;i<1000;i++)
+{
+	cache.get(i,function(data){ 
+		console.log("data:"+data+" key:"+i);
+		if(i.toString()+" processed" !== data)
+		{
+			console.log("error: wrong key-data mapping.");
+
+		}
+		if(++ctr === 1000)
+		{
+			console.log("benchmark: "+(Date.now()-t1)+" miliseconds");
+		}
+	});
+}
