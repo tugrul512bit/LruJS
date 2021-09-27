6d5e54dfb5f854d698f44e75c25a9158c67e48b1
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2021-09-27 13:07:15 +0300

    Added set method for backing-store write-caching feature.

--- lrucache.js
+++ lrucache.js
@@ -7,15 +7,20 @@ reload: evicts all cache to reload new values from backing store
 reloadKey: only evicts selected item (to reload its new value on next access)
 */
 
-let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
+let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000,callbackBackingStoreSave){
 	const me = this;
-	
+	const aTypeGet = 0;
+	const aTypeSet = 1;
 	const maxWait = elementLifeTimeMs;
 	const size = parseInt(cacheSize,10);
 	const mapping = {};
 	const mappingInFlightMiss = {};
 	const bufData = new Array(size);
 	const bufVisited = new Uint8Array(size);
+
+	// todo: add write-cache operations. set(key,value,callback) setMultiple(keys, values, callback) setAwaitable(key,value) setMultipleAwaitable(keys,values)
+	const bufEdited = new Uint8Array(size);
+
 	const bufKey = new Array(size);
 	const bufTime = new Float64Array(size);
 	const bufLocked = new Uint8Array(size);
@@ -26,6 +31,7 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 		
 		bufData[i]="";
 		bufVisited[i]=0;
+		bufEdited[i]=0;
 		bufKey[i]=rnd;
 		bufTime[i]=0;
 		bufLocked[i]=0;
@@ -33,77 +39,131 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 	let ctr = 0;
 	let ctrEvict = parseInt(cacheSize/2,10);
 	const loadData = callbackBackingStoreLoad;
+	const saveData = callbackBackingStoreSave;
 	let inFlightMissCtr = 0;
+	// refresh all items time-span in cache
 	this.reload=function(){
 		for(let i=0;i<size;i++)
 		{
 			bufTime[i]=0;
 		}
 	};
+	// refresh item time-span in cache by triggering eviction
 	this.reloadKey=function(key){
 		if(key in mapping)
 		{
 			bufTime[mapping[key]]=0;
 		}
 	};
+
+	// get value by key
 	this.get = function(keyPrm,callbackPrm){
+		// aType=0: get
+		access(keyPrm,callbackPrm,aTypeGet);
+	};
+
+	// set value by key (callback returns same value)
+	this.set = function(keyPrm,valuePrm,callbackPrm){
+		// aType=1: set
+		access(keyPrm,callbackPrm,aTypeSet,valuePrm);
+	};
+	
+	// aType=0: get
+	// aType=1: set
+	function access(keyPrm,callbackPrm,aType,valuePrm){
 		const key = keyPrm;
 		const callback = callbackPrm;
-		
+		const value = valuePrm;
 		// stop dead-lock when many async get calls are made
 		if(inFlightMissCtr>=size)
              	{
                		setTimeout(function(){
-				me.get(key,function(newData){
+				// get/set
+				access(key,function(newData){
 					callback(newData);
-				});
+				},aType,value);
 			},0);
                		return;
 	     	}
 		
-		// delay the request towards end of the cache-miss completion
+		// if key is busy, then delay the request towards end of the cache-miss completion
 		if(key in mappingInFlightMiss)
 		{
 
 			setTimeout(function(){
-				me.get(key,function(newData){
+				// get/set
+				access(key,function(newData){
 					callback(newData);
-				});
+				},aType,value);
 			},0);
 			return;
 		}
 
 		if(key in mapping)
 		{
+			// slot is an element in the circular buffer of CLOCK algorithm
 			let slot = mapping[key];
+
 			// RAM speed data
 			if((Date.now() - bufTime[slot]) > maxWait)
 			{
 				
+				// if slot is locked by another operation, postpone the current operation
 				if(bufLocked[slot])
 				{										
 					setTimeout(function(){
-						me.get(key,function(newData){
+						access(key,function(newData){
 							callback(newData);
-						});
+						},aType,value);
 					},0);
 					
 				}
-				else
+				else // slot is not locked and its lifespan has ended
 				{
-					delete mapping[key];
-					
-					me.get(key,function(newData){
-						callback(newData);
-					});
-					
+					// if it was edited, update the backing-store first
+					if(bufEdited[slot] == 1)
+					{
+						bufLocked[slot] = 1;
+						bufEdited[slot]=0;
+						mappingInFlightMiss[key] = 1; // lock key
+						inFlightMissCtr++;
+						// update backing-store, this is async
+						saveData(bufKey[slot],bufData[slot],function(){ 
+							
+							delete mapping[key]; // disable mapping for current key
+							
+							// re-simulate the access, async
+							access(key,function(newData){
+								delete mappingInFlightMiss[key];	// unlock key
+								inFlightMissCtr--;
+								bufLocked[slot] = 0;
+								callback(newData);
+							},aType,value);
+
+						});
+					}
+					else
+					{
+						delete mapping[key]; // disable mapping for current key
+						access(key,function(newData){
+							
+							callback(newData);
+						},aType,value);
+					}
 				}
 				
 			}
-			else
+			else    // slot life span has not ended
 			{
 				bufVisited[slot]=1;
 				bufTime[slot] = Date.now();
+
+				// if it is a "set" operation
+				if(aType == aTypeSet)
+				{	
+					bufEdited[slot] = 1; // later used when data needs to be written to data-store (write-cache feature)
+					bufData[slot] = value;
+				}
 				callback(bufData[slot]);
 			}
 		}
@@ -111,6 +171,8 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 		{
 			// datastore loading + cache eviction
 			let ctrFound = -1;
+			let oldVal = 0;
+			let oldKey = 0;
 			while(ctrFound===-1)
 			{
 				// give slot a second chance before eviction
@@ -127,10 +189,12 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 				// eviction conditions
 				if(!bufLocked[ctrEvict] && !bufVisited[ctrEvict])
 				{
-					// evict
+					// eviction preparations, lock the slot
 					bufLocked[ctrEvict] = 1;
 					inFlightMissCtr++;
 					ctrFound = ctrEvict;
+					oldVal = bufData[ctrFound];
+					oldKey = bufKey[ctrFound];
 				}
 
 				ctrEvict++;
@@ -140,8 +204,12 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 				}
 			}
 			
+			// user-requested key is now asynchronously in-flight & locked for other operations
 			mappingInFlightMiss[key]=1;
-			let f = function(res){
+
+			// eviction function. least recently used data is gone, newest recently used data is assigned
+			let evict = function(res){
+
 				delete mapping[bufKey[ctrFound]];
 
 				bufData[ctrFound]=res;
@@ -151,16 +219,38 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 				bufLocked[ctrFound]=0;
 
 				mapping[key] = ctrFound;
-				callback(bufData[ctrFound]);
+				callback(res);
 				inFlightMissCtr--;
-				delete mappingInFlightMiss[key];		
+				delete mappingInFlightMiss[key];
+			
 			};
-			loadData(key,f);
 
+			// if old data was edited, send it to data-store first, then fetch new data
+			if(bufEdited[ctrFound] == 1)
+			{
+				if(aType == aTypeGet)
+					bufEdited[ctrFound] = 0;
+
+				// old edited data is sent back to data-store
+				saveData(oldKey,oldVal,function(){ 
+					if(aType == aTypeGet)
+						loadData(key,evict);
+					else if(aType == aTypeSet)
+						evict(value);
+				});
+			}
+			else
+			{
+				if(aType == aTypeSet)
+					bufEdited[ctrFound] = 1;
+				if(aType == aTypeGet)
+					loadData(key,evict);
+				else if(aType == aTypeSet)
+					evict(value);	
+			}
 		}
 	};
-	
-	
+
 	this.getAwaitable = function(key){
 		return new Promise(function(success,fail){ 
 			me.get(key,function(data){
@@ -168,7 +258,7 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 			});
 		});
 	}
-	
+
 	this.getMultiple = function(callback, ... keys){
 		let result = [];
 		let ctr = keys.length;
@@ -176,9 +266,8 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 			result.push(0);
 		let ctr2 = 0;
 		keys.forEach(function(key){
-                        let ctr3=ctr2++;
 			me.get(key,function(data){
-				result[ctr3] = data;
+				result[ctr2++] = data;
 				ctr--;
 				if(ctr==0)
 				{
@@ -187,7 +276,8 @@ let Lru = function(cacheSize,callbackBackingStoreLoad,elementLifeTimeMs=1000){
 			});
 		});
 	};
-	
+
+
 	this.getMultipleAwaitable = function(... keys){
 		return new Promise(function(success,fail){
 			me.getMultiple(function(results){
